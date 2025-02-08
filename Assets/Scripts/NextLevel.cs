f3894971058c47abd64abcd25f81c61cf96a0060
Robik <robertmazurkiewicz1112@gmail.com>
2025-02-08 10:48:47 +0100

    Zdrowie

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -7,6 +7,7 @@ public class NextLevel : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {
             GameManager.instance.alreadyStarted = true;
+            GameManager.instance.playerHealth = HealthSystem.instance.currentHealth;
             SceneManager.LoadScene(sceneName);
         }
     }
